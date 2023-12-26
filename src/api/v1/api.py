7be08ef40bd12661a4d8ae8d757ccef510a769ae
Bernard Berdikul <b.berdicul@ylab.io>
2023-12-26 11:30:49 +0600

    Init

--- src/api/v1/api.py
+++ src/api/v1/api.py
@@ -3,9 +3,15 @@ from fastapi import APIRouter
 from src.api.v1.endpoints import dishes, menus, submenus
 
 api_router = APIRouter()
-api_router.include_router(menus.router, prefix="/menus", tags=["menus"])
 api_router.include_router(
-    submenus.router, prefix="/menus/{menu_id}/submenus", tags=["submenus"]
+    menus.router,
+    prefix="/menus",
+    tags=["menus"],
+)
+api_router.include_router(
+    submenus.router,
+    prefix="/menus/{menu_id}/submenus",
+    tags=["submenus"],
 )
 api_router.include_router(
     dishes.router,
